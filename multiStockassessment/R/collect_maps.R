3f5fc39bef07c01e5ea9dcf5a3d451ed8ebc7a2b
Christoffer Moesgaard Albertsen <ca@calbertsen.dk>
2019-07-04 13:01:35 +0200

    Forgot collect_maps

---
+++ multiStockassessment/R/collect_maps.R
@@ -0,0 +1,26 @@
+collect_maps <- function(x) {
+    if(class(x) != "samset")
+        stop("x must be a samset.")
+
+    
+    ncase <- length(x)
+    newpars <- multiStockassessment:::collect_pars(x)
+    singleMaps <- lapply(x,function(xx)xx$obj$env$map)
+    mapNames <- lapply(singleMaps,names)
+    uniqueNames <- unique(unlist(mapNames))
+    newmap <- lapply(newpars[uniqueNames], function(xx){
+        a <- (1:length(xx))
+        attributes(a) <- attributes(xx)
+        a
+    })
+
+    for(i in 1:ncase)
+        for(nm in mapNames[[i]]){
+            tmp <- splitParameter(newmap[[nm]])
+            tmp[[i]] <- tmp[[i]][singleMaps[[i]][[nm]]]
+            newmap[[nm]] <- combineParameter(tmp)
+        }
+    newmap <- lapply(newmap, factor)
+
+    return(newmap)
+}
